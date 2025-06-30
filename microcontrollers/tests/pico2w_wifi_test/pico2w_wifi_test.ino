6553e324ac17f831d15fa6bdf55ec126e546e721
Selena Wang <shw59@cornell.edu>
2025-06-30 14:53:28 -0400

    Create pico2w_wifi_test.ino

---
+++ microcontrollers/tests/pico2w_wifi_test/pico2w_wifi_test.ino
@@ -0,0 +1,51 @@
+/*
+  Pi Pico 2 W WiFi Station Demo
+  pico2w_wifi_test.ino
+  Use WiFi library to connect Pico 2 W to WiFi in station mode
+*/
+
+// include the WiFi Library
+#include <WiFi.h>
+
+// replace with your network credentials
+const char* ssid = "REPLACE_WITH_SSID";
+const char* password = "REPLACE_WITH_PASSWORD";
+
+void setup() {
+
+  // start the Serial Monitor
+  Serial.begin(115200);
+
+  // operate in WiFi Station mode
+  WiFi.mode(WIFI_STA);
+
+  // start WiFi with supplied parameters
+  WiFi.begin(ssid, password);
+
+  // print periods on monitor while establishing connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    delay(500);
+  }
+
+  // connection established
+  Serial.println("");
+  Serial.print("Pico 2 W is connected to WiFi network ");
+  Serial.println(WiFi.SSID());
+
+  // print IP Address
+  Serial.print("Assigned IP Address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+
+  delay(2000);
+
+  // print IP Address
+  Serial.print("Assigned IP Address: ");
+  Serial.println(WiFi.localIP());
+
+}
