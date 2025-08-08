6a3457cd617b78c7dd8ee1cb9fd49da2d9204fe6
Selena Wang <shw59@cornell.edu>
2025-08-08 01:41:30 -0400

    stuff

--- edit_graphs.py
+++ edit_graphs.py
@@ -3,20 +3,28 @@ import matplotlib.pyplot as plt
 
 # load the pickled figure
 with open("./data/figures/towing_agents_graph_2025-08-03.png.pkl", "rb") as f:
+# with open("./data/figures/object_capture_maintain_line_False_offset0_graph_2025-08-03.png.pkl", "rb") as f:
     fig = pickle.load(f)
 
-legend_colors = ["red", "yellow", "skyblue", "blue", "lightgreen"]
+# change graph colors of towing agents
+legend_colors = ["red", "yellow", "deepskyblue", "blue", "lime"]
 
 # access all axes in the figure
 for ax in fig.axes:
     # change title font size
-    # ax.title.set_fontsize(35)
+    # ax.set_title("Failed Agent X-Position Over Time")
+    # ax.set_title("Collective Radius + # of Objects Captured Over Time, No Offset")
+    ax.title.set_fontsize(30)
     ax.set_title("")
-    ax.set_ylabel("Failed Agent X-Position")
+
+    ax.set_ylabel("X-Position")
 
     # change x/y label font sizes
     ax.xaxis.label.set_fontsize(30)
     ax.yaxis.label.set_fontsize(30)
+
+    ax.set_xlim(0, 5000)
+    ax.set_ylim(-4, 30)
 
     # change tick label font sizes
     for tick in ax.xaxis.get_major_ticks():
@@ -31,9 +39,14 @@ for ax in fig.axes:
 
     # change legend font size (if it exists)
     legend = ax.get_legend()
+    handles, labels = ax.get_legend_handles_labels()
     if legend:
+        # labels for the towing agents legend
         new_labels = ["I", "II", "III", "IV", "V"]
-        ax.legend(new_labels)
+        ax.legend(handles, new_labels)
+        # legend.remove()
+        # new_labels = ["5 Obj", "10 Obj", "30 Obj", "50 Obj"]
+        # ax.legend(handles, new_labels, loc='center right', fontsize=30)
         for text in ax.get_legend().get_texts():
             text.set_fontsize(30)
 
